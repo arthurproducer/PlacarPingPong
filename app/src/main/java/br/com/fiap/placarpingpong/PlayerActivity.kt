8bc8a026d978e2d859ba92c85c7f5b09dca77ba2
Logon Aluno <logonaluno@local.com>
2019-08-13 23:04:23 -0300

    New things again

--- app/src/main/java/br/com/fiap/placarpingpong/PlayerActivity.kt
+++ app/src/main/java/br/com/fiap/placarpingpong/PlayerActivity.kt
@@ -1,12 +1,22 @@
 package br.com.fiap.placarpingpong
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_player.*
 
 class PlayerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
+
+        btStartGame.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("PLAYER_1",inputP1.text)
+            intent.putExtra("PLAYER_2",inputP2.text)
+            startActivity(intent)
+            finish()
+        }
     }
 }
